312745925ad7b7d2d43397c871e029fe3104ba8e
Ayman Mir <aymanmir@gmail.com>
2021-05-15 10:32:09 +0530

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,29 @@
+class Monster {
+  constructor(x,y,r)
+	{
+		var options = { 
+      density: 5, 
+      frictionAir: 0
+    };
+		this.x=x;
+		this.y=y;
+		this.r=r;
+		this.image=loadImage("Monster-01.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var pos=this.body.position;		
+			push()
+			translate(pos.x, pos.y-100);
+			rectMode(CENTER)
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			pop()
+			
+	}
+}
